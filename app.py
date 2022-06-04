a80ae5227626f0f88c3e642585de1dcb9a5b8352
Zjero <55556775+zjero777@users.noreply.github.com>
2022-06-04 10:08:55 +0400

    main fix add blank html template add svg icon

--- app.py
+++ app.py
@@ -10,35 +10,42 @@ app.secret_key = '&*(%&$^$&%*&(^(*&%^$&*5978539487JKHGgdsuyrh'
 
 db = SQLAlchemy(app)
 
-
 class Menu(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False, unique=True, index=True)
-    name = db.Column(db.String(30), nullable=True)
-    path = db.Column(db.String(300), nullable=True)
+    title = db.Column(db.String(30), nullable=False)
+    ref = db.Column(db.String(300))
+    priority = db.Column(db.Integer)
+    handler = db.Column(db.String(20))
+    ext_title = db.Column(db.String(80))
+    intro = db.Column(db.String(300))
+    icon_svg_name = db.Column(db.String(20))
 
     def __repr__(self):
         return '<Menu Item %r - %r - %r>' % self.id, self.name, self.path
 
-main_menu = [{'name':'Главная', 'path': '/'}, 
-             {'name':'О нас', 'path': 'about'}]
 
 
 @app.route('/home')
 @app.route('/')
 def index():
     try:
-        main_menu = Menu.query.all() 
-        
+        main_menu = Menu.query.order_by(Menu.priority.asc()).all()
     except Exception as e:
+        print(e)
         main_menu = []
-    
     return render_template("main.html", menu=main_menu)
 
+@app.route('/hot-lines')
+def hot_lines():
+    return render_template("hot-lines.html")
 
-@app.route('/about')
-def about():
-    return render_template("about.html", menu=main_menu)
+@app.route('/our-services')
+def our_services():
+    return render_template("our-services.html")
 
+@app.route('/kbt')
+def kbt():
+    return render_template("kbt.html")
 
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
